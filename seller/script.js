4e3e8aa474552c0dcd3623ac11f4421b386f1c42
unknown <mfabunan2@gmail.com>
2023-05-11 00:40:45 +0800

    first commit

---
+++ seller/script.js
@@ -0,0 +1,13 @@
+const productList = document.getElementById("product-list");
+
+function addProduct(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const description = document.getElementById("description").value;
+  const price = document.getElementById("price").value;
+  const quantity = document.getElementById("quantity").value;
+  const image = document.getElementById("image").value;
+
+ 
+}
